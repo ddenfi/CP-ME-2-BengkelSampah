990df50e1913f9f9626456fb3b785ce12225cff9
Steven-Lie <s.lie01.sl@gmail.com>
2023-10-26 18:05:17 +0700

    feat(history): show filter bottom sheet dialog after button clicked

--- app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/history/HistoryActivity.kt
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/history/HistoryActivity.kt
@@ -1,12 +1,32 @@
 package com.bengkelsampah.bengkelsampahapp.ui.history
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bengkelsampah.bengkelsampahapp.R
+import com.bengkelsampah.bengkelsampahapp.databinding.ActivityHistoryBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class HistoryActivity : AppCompatActivity() {
+    private lateinit var historyBinding: ActivityHistoryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_history)
+        historyBinding = ActivityHistoryBinding.inflate(layoutInflater)
+        setContentView(historyBinding.root)
+
+        historyBinding.btnStatusFilter.text = getString(R.string.all_status)
+        setUpFilterBottomSheet()
+    }
+
+    @SuppressLint("InflateParams")
+    private fun setUpFilterBottomSheet() {
+        val bottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet_filter, null)
+        val bottomSheetDialog = BottomSheetDialog(this)
+        bottomSheetDialog.setContentView(bottomSheetView)
+
+        historyBinding.btnStatusFilter.setOnClickListener {
+            bottomSheetDialog.show()
+        }
     }
 }
