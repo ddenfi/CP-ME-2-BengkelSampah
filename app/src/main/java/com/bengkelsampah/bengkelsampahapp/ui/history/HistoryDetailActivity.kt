e404ae50e1a29c2053cd8b04f2a7c4161cd81677
Steven-Lie <s.lie01.sl@gmail.com>
2023-10-27 18:35:58 +0700

    feat(history-detail): add waste sold adapter and show dummy history data

--- app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/history/HistoryDetailActivity.kt
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/history/HistoryDetailActivity.kt
@@ -1,12 +1,56 @@
 package com.bengkelsampah.bengkelsampahapp.ui.history
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bengkelsampah.bengkelsampahapp.R
+import com.bengkelsampah.bengkelsampahapp.databinding.ActivityHistoryDetailBinding
 
 class HistoryDetailActivity : AppCompatActivity() {
+    private lateinit var historyDetailBinding: ActivityHistoryDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_history_detail)
+        historyDetailBinding = ActivityHistoryDetailBinding.inflate(layoutInflater)
+        setContentView(historyDetailBinding.root)
+
+        setUpDetailPage()
+    }
+
+    private fun setUpDetailPage() {
+        val dummyHistoryData = DummyData.generateDummyData()
+        historyDetailBinding.apply {
+            tvStatus.text = dummyHistoryData[0].status
+
+            when (dummyHistoryData[0].status.uppercase()) {
+                "MENUNGGU KONFIRMASI" -> cardStatus.setCardBackgroundColor(Color.parseColor("#999797"))
+                "SELESAI" -> cardStatus.setCardBackgroundColor(Color.parseColor("#519B37"))
+                "DIPROSES" -> cardStatus.setCardBackgroundColor(Color.parseColor("#FFAB2A"))
+                "DIBATALKAN" -> cardStatus.setCardBackgroundColor(Color.parseColor("#E62E2E"))
+            }
+
+            tvAgentName.text = dummyHistoryData[0].agent
+            tvAgentAddress.text = dummyHistoryData[0].agentAddress
+            tvAgentPhone.text = getString(R.string.agent_phone, dummyHistoryData[0].agentPhone)
+
+            setUpWasteSold(dummyHistoryData[0].waste)
+
+            tvTotalAll.text =
+                getString(R.string.total_detail_history, dummyHistoryData[0].total.toString())
+            tvPickUpAddress.text = dummyHistoryData[0].address
+            tvPickUpDescription.text = dummyHistoryData[0].description
+        }
+    }
+
+    private fun setUpWasteSold(waste: List<DummyWaste>) {
+        val wasteSoldAdapter = WasteSoldAdapter()
+
+        historyDetailBinding.rvWasteSold.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = wasteSoldAdapter
+        }
+
+        wasteSoldAdapter.submitList(waste)
     }
 }
