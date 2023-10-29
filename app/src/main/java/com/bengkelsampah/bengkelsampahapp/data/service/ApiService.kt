bcc637420ef7fa6fb04af8f56ed239e9591d72f8
Izaz Dhiyaulhaq <id.izaz123@gmail.com>
2023-10-29 21:48:20 +0700

    feat(partner): add api service for partner end point

--- app/src/main/java/com/bengkelsampah/bengkelsampahapp/data/service/ApiService.kt
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/data/service/ApiService.kt
@@ -1,4 +1,22 @@
 package com.bengkelsampah.bengkelsampahapp.data.service
 
+import com.bengkelsampah.bengkelsampahapp.data.source.remote.response.partner.GetPartner
+import com.bengkelsampah.bengkelsampahapp.data.source.remote.response.partner.GetPartnerItem
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
+import java.util.UUID
+
 interface ApiService {
+    @GET("partners")
+    suspend fun getPartner(): GetPartner
+
+    @GET("partners/{partnerid}")
+    suspend fun getPartnerById(
+        @Path("partnerid") partnerId: UUID
+    ) : GetPartnerItem
+
+
 }
