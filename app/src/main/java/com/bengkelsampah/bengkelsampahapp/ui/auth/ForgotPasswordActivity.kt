54d0c4450649cf4af469ed6220c93496d3c0bf45
Alraz Setiawan <alrazsetiawan79@gmail.com>
2023-11-01 14:01:56 +0700

    feat(auth): implement view binding

--- app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/auth/ForgotPasswordActivity.kt
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/auth/ForgotPasswordActivity.kt
@@ -2,11 +2,14 @@ package com.bengkelsampah.bengkelsampahapp.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.bengkelsampah.bengkelsampahapp.R
+import com.bengkelsampah.bengkelsampahapp.databinding.ActivityForgotPasswordBinding
 
 class ForgotPasswordActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityForgotPasswordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/auth/LoginActivity.kt
@@ -2,11 +2,14 @@ package com.bengkelsampah.bengkelsampahapp.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.bengkelsampah.bengkelsampahapp.R
+import com.bengkelsampah.bengkelsampahapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
