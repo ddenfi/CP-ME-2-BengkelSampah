5f51e37d000990770a5d3c85066c1b38e77d274e
FAB <fajar21bahari@gmail.com>
2023-11-03 21:18:41 +0700

    Feat(Profile): UI Slicing main profile dan edit akun

---
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/profile/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.bengkelsampah.bengkelsampahapp.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bengkelsampah.bengkelsampahapp.R
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
