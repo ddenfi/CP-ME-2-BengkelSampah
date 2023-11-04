3b59ba2a15cc44eda9b9d6a809a7d0dc92349092
Steven-Lie <s.lie01.sl@gmail.com>
2023-11-04 22:55:07 +0700

    fix(history): migrate from activity to fragment

--- app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/main/screen/HistoryScreenFragment.kt
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/main/screen/HistoryScreenFragment.kt
@@ -1,24 +1,60 @@
 package com.bengkelsampah.bengkelsampahapp.ui.main.screen
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bengkelsampah.bengkelsampahapp.R
-/**
- * A simple [Fragment] subclass.
- * Use the [HistoryScreenFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+import com.bengkelsampah.bengkelsampahapp.databinding.FragmentHistoryScreenBinding
+import com.bengkelsampah.bengkelsampahapp.domain.model.DummyData
+import com.bengkelsampah.bengkelsampahapp.ui.adapter.HistoryAdapter
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
 class HistoryScreenFragment : Fragment() {
+    private var _binding: FragmentHistoryScreenBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history_screen, container, false)
+    ): View {
+        _binding = FragmentHistoryScreenBinding.inflate(inflater, container, false)
+
+        binding.btnStatusFilter.text = getString(R.string.all_status)
+        setUpFilterBottomSheet()
+        setUpHistoryList()
+
+        return binding.root
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    @SuppressLint("InflateParams")
+    private fun setUpFilterBottomSheet() {
+        val bottomSheetView = layoutInflater.inflate(R.layout.bottom_sheet_filter, null)
+        val bottomSheetDialog = BottomSheetDialog(this.requireContext())
+        bottomSheetDialog.setContentView(bottomSheetView)
+
+        binding.btnStatusFilter.setOnClickListener {
+            bottomSheetDialog.show()
+        }
+    }
+
+    private fun setUpHistoryList() {
+        val historyAdapter = HistoryAdapter()
+        val dummyHistoryData = DummyData.generateDummyData()
+
+        binding.rvHistory.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = historyAdapter
+        }
+
+        historyAdapter.submitList(dummyHistoryData)
+    }
 }
