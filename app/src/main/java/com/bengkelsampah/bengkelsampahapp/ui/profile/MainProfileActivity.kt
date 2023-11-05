2fb7bc54186559dc83d5acbb26986237e573eec9
FAB <fajar21bahari@gmail.com>
2023-11-05 21:45:51 +0700

    Feat(Profile): Change UI EditText and Add WebView

--- app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/profile/MainProfileActivity.kt
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/profile/MainProfileActivity.kt
@@ -3,10 +3,12 @@ package com.bengkelsampah.bengkelsampahapp.ui.profile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bengkelsampah.bengkelsampahapp.R
+import com.bengkelsampah.bengkelsampahapp.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_profile)
     }
 }
