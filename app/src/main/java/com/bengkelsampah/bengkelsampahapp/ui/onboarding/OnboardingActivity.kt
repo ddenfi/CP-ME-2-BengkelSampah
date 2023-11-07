90d315398ec658f258ad6386aa733f559e10b2da
Deki NFi <deki.nfi@gmail.com>
2023-11-07 23:05:12 +0700

    fix(onboarding): bug scroll state

--- app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/onboarding/OnboardingActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.viewpager2.widget.ViewPager2
 import com.bengkelsampah.bengkelsampahapp.databinding.ActivityOnboadingBinding
 import com.bengkelsampah.bengkelsampahapp.domain.model.OnboardingPageModel
 import com.bengkelsampah.bengkelsampahapp.ui.adapter.OnboardingAdapter
@@ -24,12 +25,24 @@ class OnboardingActivity : AppCompatActivity() {
         setUpOnboardingView()
     }
 
+
     private fun setUpOnboardingView() {
         val adapter = OnboardingAdapter()
         binding.vpOnboard.adapter = adapter
         binding.indicatorOnboard.attachTo(binding.vpOnboard)
 
-
+        binding.vpOnboard.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (position == itemOnboarding.size - 1) {
+                    binding.btnOnboardFinish.visibility = View.VISIBLE
+                    binding.ctnOnboardNav.visibility = View.GONE
+                } else {
+                    binding.btnOnboardFinish.visibility = View.GONE
+                    binding.ctnOnboardNav.visibility = View.VISIBLE
+                }
+            }
+        })
 
         binding.btnOnboardFinish.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
@@ -44,11 +57,6 @@ class OnboardingActivity : AppCompatActivity() {
             if (binding.vpOnboard.currentItem < itemOnboarding.size - 1) {
                 binding.vpOnboard.currentItem = binding.vpOnboard.currentItem + 1
             }
-            if (binding.vpOnboard.currentItem == itemOnboarding.size - 1) {
-                binding.btnOnboardFinish.visibility = View.VISIBLE
-                binding.ctnOnboardNav.visibility = View.GONE
-            } else binding.btnOnboardFinish.visibility =
-                View.GONE
         }
     }
 }
