7a9202252e05cdde6014d22f7430d0c7474c1bf4
FAB <fajar21bahari@gmail.com>
2023-11-08 09:27:50 +0700

    change UI edittext on edit profile and add webview

--- app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/profile/PrivacyPolicyProfileActivity.kt
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/profile/PrivacyPolicyProfileActivity.kt
@@ -3,10 +3,13 @@ package com.bengkelsampah.bengkelsampahapp.ui.profile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bengkelsampah.bengkelsampahapp.R
+import com.bengkelsampah.bengkelsampahapp.databinding.ActivityPrivacyPolicyProfileBinding
 
 class PrivacyPolicyProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPrivacyPolicyProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_privacy_policy_profile)
+        binding = ActivityPrivacyPolicyProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
