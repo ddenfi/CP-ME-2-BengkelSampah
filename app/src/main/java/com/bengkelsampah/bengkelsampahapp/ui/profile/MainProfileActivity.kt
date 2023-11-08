89c1d98fae5ef4a307a5897530499323b27ae373
FAB <fajar21bahari@gmail.com>
2023-11-08 09:40:45 +0700

    Revert "change UI edittext on edit profile and add webview"
    This reverts commit 7a9202252e05cdde6014d22f7430d0c7474c1bf4.

--- app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/profile/MainProfileActivity.kt
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/profile/MainProfileActivity.kt
@@ -2,13 +2,13 @@ package com.bengkelsampah.bengkelsampahapp.ui.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bengkelsampah.bengkelsampahapp.R
 import com.bengkelsampah.bengkelsampahapp.databinding.ActivityProfileBinding
 
-class MainProfileActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityProfileBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_profile)
     }
 }
--- app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/profile/PrivacyPolicyProfileActivity.kt
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/profile/PrivacyPolicyProfileActivity.kt
@@ -3,13 +3,10 @@ package com.bengkelsampah.bengkelsampahapp.ui.profile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bengkelsampah.bengkelsampahapp.R
-import com.bengkelsampah.bengkelsampahapp.databinding.ActivityPrivacyPolicyProfileBinding
 
 class PrivacyPolicyProfileActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityPrivacyPolicyProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPrivacyPolicyProfileBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_privacy_policy_profile)
     }
 }
