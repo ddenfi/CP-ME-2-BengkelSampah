b804c100fa897846dca031c7fa6cf8175ff8df99
Deki NFi <deki.nfi@gmail.com>
2023-11-12 21:35:01 +0700

    feat(Pickup-waste): add UI pickup waste *base

--- app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/driver/HomeDriverFragment.kt
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/driver/HomeDriverFragment.kt
@@ -1,5 +1,6 @@
 package com.bengkelsampah.bengkelsampahapp.ui.driver
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.bengkelsampah.bengkelsampahapp.R
 import com.bengkelsampah.bengkelsampahapp.databinding.FragmentHomeDriverBinding
+import com.bengkelsampah.bengkelsampahapp.ui.moneybag.MoneyBagActivity
+import com.bengkelsampah.bengkelsampahapp.ui.pickupwaste.PickupActivity
 
 
 class HomeDriverFragment : Fragment() {
@@ -16,13 +19,29 @@ class HomeDriverFragment : Fragment() {
 
     private val viewModel: DriverMainViewModel by viewModels()
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeDriverBinding.inflate(inflater, container, false)
+
+        setupNavigation()
         return binding.root
+    }
+
+    private fun setupNavigation() {
+        binding.btnDriverHomeJemputSampah.setOnClickListener {
+            val intent = Intent(activity,PickupActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnDriverHomeArtikel.setOnClickListener {
+            //TODO Connect to article list
+        }
+        binding.btnDriverHomeMoneyBag.setOnClickListener {
+            val intent = Intent(activity,MoneyBagActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onDestroy() {
