e7ae78fecdeadd6533759fb84c6c4bcbb1869215
Alraz Setiawan <alrazsetiawan79@gmail.com>
2023-11-13 23:11:16 +0700

    refactor(order): change fragment to activity

---
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/ui/formorder/FormOrderActivity.kt
@@ -0,0 +1,13 @@
+package com.bengkelsampah.bengkelsampahapp.ui.formorder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bengkelsampah.bengkelsampahapp.R
+
+class FormOrderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_order)
+    }
+}
