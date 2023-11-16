b4619165c1f4929d749a26c0c8445e4a16c96ac4
Alraz Setiawan <alrazsetiawan79@gmail.com>
2023-11-16 18:28:02 +0700

    refactor(api call): change base url

--- app/src/main/java/com/bengkelsampah/bengkelsampahapp/data/source/remote/network/ApiClient.kt
+++ app/src/main/java/com/bengkelsampah/bengkelsampahapp/data/source/remote/network/ApiClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "https://bengkelsampah.com/"
+    private const val BASE_URL = "https://bengkelsampahauth.cyclic.app/"
 
     private val logging: HttpLoggingInterceptor
         get() {
